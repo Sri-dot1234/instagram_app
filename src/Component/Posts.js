075fd34151786be1469ff6c7b2f4b83b8502c49c
Srivani Vallala <sv21272@corp.ojas-it.com>
2022-04-28 19:05:52 +0530

    insta commit

--- src/Component/Posts.js
+++ src/Component/Posts.js
@@ -1,92 +1,77 @@
 import React, { useEffect, useState } from "react";
 import "./Posts.css";
-import firebase from "firebase"
-import { db } from "./firebase";
+import { db, timestamp } from "./firebase";
 import Avatar from "@material-ui/core/Avatar";
 function Posts({ postId, username, caption, imageUrl }) {
-const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState([]);
   const [comment, setComment] = useState("");
 
-  
   const user = localStorage.getItem("login");
   useEffect(() => {
-  
-    let unsubscribe= db
-        .collection("posts")
-        .doc(postId)
-        .collection("comments")
-        .orderBy('timestamp','desc')
-        .onSnapshot((snapshot) => {
-          setComments(snapshot.docs.map((doc) => doc.data()));
-        });
-    
+    let unsubscribe = db
+      .collection("posts")
+      .doc(postId)
+      .collection("comments")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setComments(snapshot.docs.map((doc) => doc.data()));
+      });
     return () => {
       unsubscribe();
     };
-
-  }, [postId]);
+  }, []);
   const postComment = (event) => {
-      if(postId){
+    if (postId) {
+      const createdAt = timestamp;
       event.preventDefalut();
-      db.collection("posts").doc(postId)
-      .collection("comments").add({
-       text:comment,
-       username:user.displayName,
-       timestamp:firebase.firestore.FieldValue.serverTimestamp(),  
-      })
+      db.collection("posts").doc(postId).collection("comments").add({
+        text: comment,
+        username: user,
+        createdAt: createdAt,
+      });
     }
-      setComment('');
+    setComment("");
   };
-  const deleteuser=(Id)=>{
-  //  console.log(Id)
-//    db.collection("posts").doc(Id).delete();
-}
+  const deleteuser = (Id) => {
+    console.log(Id);
+    //  db.collection("posts").doc(Id).delete();
+  };
   return (
-      <div>
-
-     <div className="posts">
-       
-      <div className="post_header">
-        <Avatar
-          className="post_avatar"
-          alt="srivani"
-          src=""
-        />
-        <h3>{username}</h3> <h6 className="float-right del" onClick={deleteuser(postId)}>delete</h6>
+    <div>
+      <div className="posts">
+        <div className="post_header">
+          <Avatar className="post_avatar" alt="srivani" src="" />
+          <h3>{username}</h3>{" "}
+          <h6 className="float-right del" onClick={deleteuser(postId)}>
+            delete
+          </h6>
+        </div>
+        <img className="post_image" src={imageUrl} alt="img" />
+        <h4 className="post_text">
+          {username}:<strong>{caption}</strong>
+        </h4>
+        <div className="post_comments">
+          {comments.map((com) => (
+            <p>
+              <b>{com.text}</b>
+            </p>
+          ))}
+        </div>
+        {user && (
+          <form className="post_commentBox">
+            <input
+              className="post_input"
+              type="text"
+              placeholder="Add a comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button className="post_button" type="submit" onClick={postComment}>
+              post
+            </button>
+          </form>
+        )}
       </div>
-      <img className="post_image" src={imageUrl} alt="img" />
-      <h4 className="post_text">
-        {username}: 
-        <strong>{caption}</strong>
-      </h4>
-      <div className="post_comments">
-         
-        {comments.map((com) => (
-          
-        <p><b>{com.text}</b></p>
-        ))}
-      </div>
-      {user && (
-      <form className="post_commentBox">
-        <input
-          className="post_input"
-          type="text"
-          placeholder="Add a comment"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <button
-          className="post_button"
-         
-          type="submit"
-          onClick={postComment}
-        >
-          post
-        </button>
-      </form> )}
-    </div>
-
-    
     </div>
   );
 }
--- src/Component/ProgressBar.js
+++ src/Component/ProgressBar.js
@@ -1,23 +1,20 @@
-import React, { useEffect } from 'react'
-import useStorage from './useStorage'
-const ProgressBar = ({file ,setfile ,caption }) => {
-    const {url,progress}=useStorage(file)
-   // console.log(progress , url)
+import React, { useEffect } from "react";
+import useStorage from "./useStorage";
+const ProgressBar = ({ file, setfile, caption }) => {
+  const { url, progress } = useStorage(file);
+  // console.log(progress , url)
 
-    useEffect(
-        ()=>{
-            if(url){
-           setfile(url);
-
-            }
-        },[url,setfile]
-    )
+  useEffect(() => {
+    if (url) {
+      setfile(url);
+    }
+  }, [url, setfile]);
 
   return (
-    <div className='progress-bar' style={{width:progress + "%"}}>progress
-
+    <div className="progress-bar" style={{ width: progress + "%" }}>
+      progress
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
